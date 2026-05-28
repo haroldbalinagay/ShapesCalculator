caf74f85a2062e9ba13394d8b21d1a42ba0f0db7
HaroldBalinagay <harold_balinagay@dlsu.edu.ph>
2026-05-28 13:01:50 +0800

    Created square class

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,22 @@
 public class Square {
+    double side;
+    Square(double side) {
+        setSide(side);
+    }
+    double getSide() {
+        return side;
+    }
+    void setSide(double side){
+    if (side <= 0){
+        IO.println("Error side must be a positive number");
+        return;
+    }
+    this.side = side;
+    }
+    double calculateArea(){
+        return side * side;
+    }
+    double calculateperimeter(){
+        return 4 * side;
+    }
 }
