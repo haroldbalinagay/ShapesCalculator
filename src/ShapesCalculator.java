25541e23cfe32313d7bc64216556f5eb0ee538b6
HaroldBalinagay <harold_balinagay@dlsu.edu.ph>
2026-05-31 21:46:43 +0800

    Test file to try and run the other classes

---
+++ src/ShapesCalculator.java
@@ -0,0 +1,20 @@
+public class ShapesCalculator {
+    void main(){
+        Square testSquare = new Square(2.6);
+        IO.println(testSquare.getSide());
+        IO.println(testSquare.calculateArea());
+        IO.println(testSquare.calculatePerimeter());
+        Rectangle testRectangle = new Rectangle(2.4,6.7);
+        IO.println(testRectangle.getHeight());
+        IO.println(testRectangle.getWidth());
+        IO.println(testRectangle.calculateArea());
+        IO.println(testRectangle.calculatePerimeter());
+        double testRadius = 3.14;
+        Circle testCirlce = new Circle(testRadius);
+        IO.println(testCirlce.getRadius());
+        IO.println(testCirlce.calculateArea());
+        IO.println(testCirlce.calculatePerimeter());
+        IO.println(testCirlce.calculateDiameter());
+    }
+}
+
