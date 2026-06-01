ed02ff84b4da280eab8a3ffeafe27a05748958b3
HaroldBalinagay <harold_balinagay@dlsu.edu.ph>
2026-06-01 12:09:01 +0800

    Finished Cube class

---
+++ src/Cube.java
@@ -0,0 +1,22 @@
+public class Cube{
+    double side;
+    Cube(double side) {
+        setSide(side);
+    }
+    double getSide() {
+        return side;
+    }
+    void setSide(double side){
+        if (side <= 0){
+            IO.println("Error side must be a positive number");
+            return;
+        }
+        this.side = side;
+    }
+    double calculateSurfaceArea(){
+        return 6 * side * side;
+    }
+    double calculateVolume(){
+        return side * side * side;
+    }
+}
