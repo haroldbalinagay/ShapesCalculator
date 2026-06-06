fdc925bd9cbac79b17a7dccea407d94d0e6c723e
HaroldBalinagay <harold_balinagay@dlsu.edu.ph>
2026-06-06 20:48:00 +0800

    Finished print circle method

--- src/Circle.java
+++ src/Circle.java
@@ -30,4 +30,36 @@ public class Circle {
     public double calculateDiameter(){
         return radius *2;
     }
+    public void printCircle(){
+        if (filled) {
+            IO.println("Filled Circle, radius = " + Math.round(radius) + ":");
+        }
+        else {
+            IO.println("Hollow Circle, radius = " + Math.round(radius) + ":");
+        }
+
+        int r = (int) Math.round(radius);
+
+        for (int i = -r; i <= r; i++) {
+            for (int j = -r; j <= r; j++) {
+                double x = j;
+                double y = i;
+                double distanceSq = (x * x) + (y * y);
+                if (filled) {
+                    if (distanceSq <= (r * r)) {
+                        IO.print("* ");
+                    } else {
+                        IO.print("  ");
+                    }
+                } else {
+                    if (distanceSq <= (r * r) && distanceSq >= (r - 1) * (r - 1)) {
+                        IO.print("* ");
+                    } else {
+                        IO.print("  ");
+                    }
+                }
+            }
+            IO.println("");
+        }
+    }
 }
