b125d3fb963914ff8e79e71109d00db9fe1214b2
HaroldBalinagay <harold_balinagay@dlsu.edu.ph>
2026-06-06 23:55:20 +0800

    fixed the loop

--- src/ShapesCalculator.java
+++ src/ShapesCalculator.java
@@ -1,19 +1,21 @@
 public class ShapesCalculator {
     void main(){
         int choice = 0;
-        while (choice !=3){
+        boolean run = true;
+        while (run){
             IO.println("=====================================================");
             IO.println("                GEOMETRY CALCULATOR");
             IO.println("=====================================================");
             IO.println("1. 2D Shapes Calculator");
-            IO.println("1. 3D Shapes Calculator");
+            IO.println("2. 3D Shapes Calculator");
             IO.println("3. Exit");
             IO.println("=====================================================");
-            choice = Integer.parseInt(IO.readln("Enter your choice (1-3)"));
+            choice = Integer.parseInt(IO.readln("Enter your choice (1-3) "));
             switch (choice){
-                case 1:twoDShapes();
+                case 1:twoDShapes(); break;
                 //case 2:threeDShapes();
-                case 3: break;
+                case 3: run = false; break;
+                default: IO.println("Please enter a valid input");
             }
         }
     }
@@ -21,11 +23,11 @@ public class ShapesCalculator {
         IO.println("=====================================================");
         IO.println("                2D Shapes Calculator");
         IO.println("=====================================================");
-        IO.println("1. 2D Shapes Calculator");
-        IO.println("1. 3D Shapes Calculator");
-        IO.println("3. Exit");
-        IO.println("4. 2D Shapes Calculator");
-        IO.println("5. 3D Shapes Calculator");
+        IO.println("1. Square");
+        IO.println("2. Rectangle");
+        IO.println("3. Circle");
+        IO.println("4. Ellipse");
+        IO.println("5. Triangle");
         IO.println("6. Exit");
     }
 }
